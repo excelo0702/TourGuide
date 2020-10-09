3d390bed10fa98220049130e32d0c9a788434551
Tanish <tanishy0702@gmail.com>
2020-10-09 13:32:54 +0530

    Splash Screen Fixed

--- app/src/main/java/com/example/tourguide/SplashScreenActivity.java
+++ app/src/main/java/com/example/tourguide/SplashScreenActivity.java
@@ -12,9 +12,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
         LogoLauncher logoLauncher= new LogoLauncher();
